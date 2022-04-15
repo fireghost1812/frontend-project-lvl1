130efea738d3fa096cc13b87130058636e4410cb
Иван Огурцов <IOgurtsov@mont.com>
2022-04-15 23:02:01 +0300

    step4 linters actions

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
 export default () => {
@@ -5,4 +6,3 @@ export default () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-
